e653c91525b9e2373e787fc9c7e41727289dfc5d
SamuelShiferaw <samuelshiferaw.info@gmail.com>
2025-05-08 12:48:48 +0300

    1-twitter-clone-backend

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+import dotenv from "dotenv";
+import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.routes.js";
+import { v2 as cloudinary } from "cloudinary";
+import postRoutes from "./routes/post.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // used to parse req.bodu from auth.controller.js
+app.use(express.urlencoded({ extended: true })); // used to parse from data(urlencoded)
+app.use(cookieParser());
+
+// console.log(process.env.MONGO_URI);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectMongoDB();
+});
