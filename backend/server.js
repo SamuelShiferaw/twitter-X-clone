fd6d6bc40f9ba293c66c0b3daf70cf59baaebb4b
SamuelShiferaw <samuelshiferaw.info@gmail.com>
2025-05-29 11:08:46 +0300

    3-Front-and-Back Connected

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import userRoutes from "./routes/user.routes.js";
 import { v2 as cloudinary } from "cloudinary";
 import postRoutes from "./routes/post.routes.js";
 import notificationRoutes from "./routes/notification.routes.js";
+import path from "path";
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" })); // used to parse req.bodu from auth.controller.js
 app.use(express.urlencoded({ extended: true })); // used to parse from data(urlencoded)
@@ -29,6 +31,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
